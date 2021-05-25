1503a68524e0ed109bbb7a56b8c4bab1830fa814
asahi <asap1017@docomo.ne.jp>
2021-05-25 10:36:53 +0900

    イベントモデル作成 #5

---
+++ app/models/event.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint           not null, primary key
+#  date       :date
+#  name       :string           not null
+#  store      :string
+#  text       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_events_on_user_id  (user_id)
+#
+class Event < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_011103) do
+ActiveRecord::Schema.define(version: 2021_05_24_231330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "text", null: false
+    t.string "store"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
