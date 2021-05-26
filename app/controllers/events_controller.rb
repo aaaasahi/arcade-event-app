99eddd55d973ea2cd9c269be6ef6133916002db1
asahi <asap1017@docomo.ne.jp>
2021-05-26 14:26:40 +0900

    controller作成 #6

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
   def index
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to events_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :text, :store, :date)
+  end
 end
