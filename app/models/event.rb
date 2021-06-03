108706a4b7ff772e34e13ff43045ffad4b622f69
asahi <asap1017@docomo.ne.jp>
2021-06-03 12:58:45 +0900

    active_hashのモデルの追加 #19

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,6 +18,10 @@
 #  index_events_on_user_id  (user_id)
 #
 class Event < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+
   belongs_to :user
   has_one_attached :eyecatch
 
