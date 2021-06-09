2a340efcb5cd0e6bdd62ce042513ad0ab1b22d05
asahi <asap1017@docomo.ne.jp>
2021-06-09 09:52:25 +0900

    コメント追加できるように #33

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,26 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
-    article = Article.find(params[:article_id])
-    @comment = article.comments.build
+    event = Event.find(params[:event_id])
+    @comment = event.comments.build
+    @comment.user = current_user
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    @comment = event.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to event_path(event), notice: 'コメントを追加'
+    else
+      flash.now[:error] = 'コメント出来ませんでした'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -22,4 +22,6 @@
 class Comment < ApplicationRecord
   belongs_to :event
   belongs_to :user
+  
+  validates :content, presence: true
 end
