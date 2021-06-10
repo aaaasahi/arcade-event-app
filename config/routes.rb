b5187319096c1411a3d9f7c26c48e6c19bae1bc3
asahi <asap1017@docomo.ne.jp>
2021-06-11 08:29:09 +0900

    user詳細をaccountに変更 #39

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     controllers: { registrations: 'registrations' }
   root 'events#index'
   get 'events/search', to: 'events#search'
-  get '/users/:id', to: 'users#show', as: 'user'
   
   resources :events do
     resources :comments, only: [:new, :create]
@@ -11,6 +10,8 @@ Rails.application.routes.draw do
     resource :clip, only: [:create, :destroy]
     resource :join, only: [:create, :destroy]
   end
+
+  resources :accounts, only: [:show]
 
   resource :profile, only: [:show, :edit, :update]
   resources :clip_events, only: [:index]
