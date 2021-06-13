7b9f3d3683c3dc83eaf1917793adcf1bf2a30abc
asahi <asap1017@docomo.ne.jp>
2021-06-13 12:51:43 +0900

    clipの表示切り替え #41

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,12 +8,26 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap/dist/js/bootstrap"
 import $ from 'jquery'
+import axios from 'axios'
 
 Rails.start()
 ActiveStorage.start()
 
+const handleClipDisplay = (hasClipped) => {
+  if (hasClipped) {
+    $('.active-clip').removeClass('hidden')
+  } else {
+    $('.inactive-clip').removeClass('hidden')
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  $('.title').on('click', () => {
-    window.alert('CLICKED')
-  })
+  const dataset = $('#event-show').data()
+  const eventId = dataset.eventId
+  axios.get(`/events/${eventId}/clip`)
+    .then((response) => {
+      console.log(response)
+      const hasClipped = response.data.hasClipped
+      handleClipDisplay(hasClipped)
+    })
 })
