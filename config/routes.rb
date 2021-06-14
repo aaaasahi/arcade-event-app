de9dbdbaee79b2776758b5d0a09d9585a1e6b077
asahi <asap1017@docomo.ne.jp>
2021-06-15 08:18:39 +0900

    ゲストログイン機能の追加 #49

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   root 'events#index'
   get 'events/search', to: 'events#search'
   
