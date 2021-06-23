17c686a1aa93dd6adcb306f3cf474a066da7e136
asahi <asap1017@docomo.ne.jp>
2021-06-23 16:53:08 +0900

    プロフ部分の言語変更 #48

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -1,7 +1,7 @@
 module ProfileDecorator
 
   def age_cal
-    return '未設定' unless age.present?
+    return I18n.t('profiles.not-set') unless age.present?
     years = Time.zone.now.year - age.year
     days = Time.zone.now.yday - age.yday
     if days < 0
