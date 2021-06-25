9478e6c3091c1a34fe6d88c9c6f44de9c5fca19b
asahi <asap1017@docomo.ne.jp>
2021-06-25 14:56:44 +0900

    adminユーザーのみ #65

--- app/controllers/administrator/events_data_controller.rb
+++ app/controllers/administrator/events_data_controller.rb
@@ -1,4 +1,10 @@
 class Administrator::EventsDataController < ApplicationController
+  before_action :admin_user
   def index
   end
+  
+  private
+    def admin_user
+      redirect_to(root_url) unless current_user.administrator?
+    end
 end
