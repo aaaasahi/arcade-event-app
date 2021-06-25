83381b21c720122b6111d84bf047eb16e9915a11
asahi <asap1017@docomo.ne.jp>
2021-06-25 14:57:23 +0900

    ユーザーによって変更 #65

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,18 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }
   end
 
+
+  def after_sign_in_path_for(resource)
+    if current_user.administrator?
+      administrator_admins_path 
+    else 
+      root_path
+    end
+  end 
+
   private
-  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+
 end
