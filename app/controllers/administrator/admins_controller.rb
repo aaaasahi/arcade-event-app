1d4acf1e316f4e07bb46886b8d1f986ee2472284
asahi <asap1017@docomo.ne.jp>
2021-06-27 17:17:20 +0900

    admin top page Chartを表示 #66

--- app/controllers/administrator/admins_controller.rb
+++ app/controllers/administrator/admins_controller.rb
@@ -1,6 +1,20 @@
 class Administrator::AdminsController < ApplicationController
   before_action :admin_user
   def index
+    @users = User.all 
+    @user_week = User.week
+    @user_yesterday = User.yesterday
+
+
+    @events = Event.all
+    @event_week = Event.week
+    @event_yesterday = Event.yesterday
+
+
+    @music = Event.where(category_id: 1).count
+    @video = Event.where(category_id: 2).count
+    @card = Event.where(category_id: 3).count
+    @other = Event.where(category_id: 4).count
   end
   
   private
