19acda12cedbff487f8c8872b51b121bf24f5ee0
asahi <asap1017@docomo.ne.jp>
2021-07-07 11:17:06 +0900

    不具合修正 #70

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,12 +6,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
-set :output, "#{Rails.root}/log/cron.log"
+set :output, "log/cron.log"
 
-every 1.day, at: '8am' do
+every 1.day, at: '8:00 am' do
   rake 'admin_report:mail_admin_report'
 end
 
-every 1.day, at: '8am' do
+every 1.day, at: '8:00 am' do
   rake 'close_event:close_event'
 end
