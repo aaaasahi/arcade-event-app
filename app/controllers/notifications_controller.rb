7abe8627476f7976d6106eda2644adb29091eb8f
asahi <asap1017@docomo.ne.jp>
2021-07-14 13:21:12 +0900

    通知一覧にページネーション #78

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,9 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
+  PER_PAGE = 5
 
   def index
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.page(params[:page]).per(PER_PAGE)
   end
 
   def update
