18ff769ba6607c24376c88584622a5671efb5135
asahi <asap1017@docomo.ne.jp>
2021-08-04 12:18:03 +0900

    プロフィール部分のN+1を修正 #80

--- app/controllers/clip_events_controller.rb
+++ app/controllers/clip_events_controller.rb
@@ -2,6 +2,6 @@ class ClipEventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @events = current_user.clip_events
+    @events = current_user.clip_events.includes(:tags, :tagmaps).with_attached_eyecatch
   end
 end
--- app/controllers/join_events_controller.rb
+++ app/controllers/join_events_controller.rb
@@ -2,6 +2,6 @@ class JoinEventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @events = current_user.join_events
+    @events = current_user.join_events.includes(:tags, :tagmaps).with_attached_eyecatch
   end
 end
