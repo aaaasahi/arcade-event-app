149d7b2ce4d37b02b6c3e30ec771dcad82ff523d
asahi <asap1017@docomo.ne.jp>
2021-08-30 06:24:23 +0900

    whenever production #104

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,3 +45,5 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_module
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :whenever_roles,        ->{ :app }
